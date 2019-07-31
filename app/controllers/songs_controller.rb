94af8c8c4d58d351a384c9105774a12b9b9eb5d4
Natalie <natsetc@gmail.com>
2019-07-30 18:18:41 -0700

    refactor and notes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,6 +16,7 @@ class SongsController < ApplicationController
         @song = Song.new(song_params)
         if @song.save
             redirect_to song_path(@song)
+            # redirect_to @song
         else
             render :new
         end
@@ -27,6 +28,7 @@ class SongsController < ApplicationController
     def update
         if @song.update(song_params)
             redirect_to song_path(@song)
+            # redirect_to @song
         else
             render :edit
         end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
-    validates :title, presence: true
-    validates :title, uniqueness: { scope: [:release_year, :artist_name] }
+    validates :title, presence: true, uniqueness: { scope: [:release_year, :artist_name] }
+    # validates :title, uniqueness: { scope: %i[release_year artist_name], message: 'cannot be repeated by the same artist in the same year' }
 
     validates :released, inclusion: { in: [true, false] }
 
